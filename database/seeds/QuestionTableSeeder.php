d7043af4afc8c85517a2a7e086f4f8b9ef8ac94e
EL KHADDAR ZAKARIA <elkhaddar.zakaria@hotmail.fr>
2019-08-26 12:27:42 +0200

    cinquième commit modif creation des seeders question&sondage

--- database/seeds/QuestionTableSeeder.php
+++ database/seeds/QuestionTableSeeder.php
@@ -10,122 +10,127 @@ class QuestionTableSeeder extends Seeder
      * @return void
      */
     public function run()
-    {
-        <?php
-
-use Illuminate\Database\Seeder;
-
-class QuestionsTableSeeder extends Seeder
-{
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
     {       
     	DB::table('questions')->insert([
             'corps'=>'Votre email',
             'type'=>'B',
-            'choix'=>'' 	
+            'choix'=>'',
+            'sondage_id'=>1 	
         ]);
         DB::table('questions')->insert([
             'corps'=>'Votre âge',
             'type'=>'B',
-            'choix'=>''   
+            'choix'=>'',
+            'sondage_id'=>1   
         ]);
         DB::table('questions')->insert([
             'corps'=>'Votre sexe',
             'type'=>'A',
-            'choix'=>json_encode(["Homme","Femme","Préfère ne pas répondre"])   
+            'choix'=>json_encode(["Homme","Femme","Préfère ne pas répondre"]),
+            'sondage_id'=>1   
         ]);
         DB::table('questions')->insert([
             'corps'=>'Nombre de presonne dans votre foyer (adulte & enfants )',
             'type'=>'C',
-            'choix'=>''   
+            'choix'=>'',
+            'sondage_id'=>1   
         ]);
         DB::table('questions')->insert([
             'corps'=>'Votre profession',
             'type'=>'B',
-            'choix'=>''   
+            'choix'=>'',
+            'sondage_id'=>1   
         ]);
         DB::table('questions')->insert([
             'corps'=>'Quel marque de casque VR utilisez vous?',
             'type'=>'A',
-            'choix'=>json_encode(['Occulus Rifts/s', 'HTC Vive', 'Windows Mixed Reality', 'PSVR'])   
+            'choix'=>json_encode(['Occulus Rifts/s', 'HTC Vive', 'Windows Mixed Reality', 'PSVR']),
+            'sondage_id'=>1   
         ]);
         DB::table('questions')->insert([
             'corps'=>"Sur quel magasin d'application achetez vous des contenus VR?",
             'type'=>'A',
-            'choix'=>json_encode(['SteamVR','Occulus store', 'Viveport', 'Playstation VR', 'Google Play', 'Windows store'])   
+            'choix'=>json_encode(['SteamVR','Occulus store', 'Viveport', 'Playstation VR', 'Google Play', 'Windows store']),
+            'sondage_id'=>1   
         ]);
         DB::table('questions')->insert([
             'corps'=>"Quel casque envisagez vous d'acheter dans un futur proche?",
             'type'=>'A',
-            'choix'=>json_encode(['Occulus Quest', 'Occulus Go', 'HTC Vive Pro', 'Autre', 'Aucun'])   
+            'choix'=>json_encode(['Occulus Quest', 'Occulus Go', 'HTC Vive Pro', 'Autre', 'Aucun']),
+            'sondage_id'=>1   
         ]);
         DB::table('questions')->insert([
             'corps'=>'Au sein de votre foyer, combien de personne utilisent votre casque VR pour regarder Bigscreen?',
             'type'=>'C',
-            'choix'=>''   
+            'choix'=>'',
+            'sondage_id'=>1   
         ]);
         DB::table('questions')->insert([
             'corps'=>'Vous utilisez principalement Bigscreen pour:',
             'type'=>'A',
-            'choix'=>json_encode(['regarder des émissions TV en direct','regarder des films','jouer en solo','jouer en team'])  
+            'choix'=>json_encode(['regarder des émissions TV en direct','regarder des films','jouer en solo','jouer en team']),
+            'sondage_id'=>1  
         ]);
         DB::table('questions')->insert([
             'corps'=>"Combien donnez vous de point pour la qualité de l'image sur Bigscreen?",
             'type'=>'C',
-            'choix'=>''   
+            'choix'=>'',
+            'sondage_id'=>1   
         ]);
         DB::table('questions')->insert([
             'corps'=>"Combien donnez vous de point pour le confort d'utilisation de l'interface Bigscreen?",
             'type'=>'C',
-            'choix'=>''   
+            'choix'=>'',
+            'sondage_id'=>1   
         ]);
         DB::table('questions')->insert([
             'corps'=>'Combien donnez vous de point pour la connection réseau de Bigscreen?',
             'type'=>'C',
-            'choix'=>''   
+            'choix'=>'',
+            'sondage_id'=>1   
         ]);
         DB::table('questions')->insert([
             'corps'=>'Combien donnez vous de point pour la qualité des graphismes 3D dans Bigscreen?',
             'type'=>'C',
-            'choix'=>''   
+            'choix'=>'',
+            'sondage_id'=>1   
         ]);
         DB::table('questions')->insert([
             'corps'=>'Combien donnez vous de point pour la qualité audio dans Bigscreen?',
             'type'=>'C',
-            'choix'=>''   
+            'choix'=>'',
+            'sondage_id'=>1   
         ]);
         DB::table('questions')->insert([
             'corps'=>'Aimeriez vous avoir des notifications plus précises au cours de vos sessions Bigscreen?',
             'type'=>'A',
-            'choix'=>json_encode(['Oui','Non'])  
+            'choix'=>json_encode(['Oui','Non']),
+            'sondage_id'=>1  
         ]);
         DB::table('questions')->insert([
             'corps'=>'Aimeriez vous pouvoir inviter un ami à rejoindre votre session via son smartphone?',
             'type'=>'A',
-            'choix'=>json_encode(['Oui','Non'])   
+            'choix'=>json_encode(['Oui','Non']),
+            'sondage_id'=>1   
         ]);
         DB::table('questions')->insert([
             'corps'=>'Aimeriez vous pourvoir enregistrer des émissions TV pour pouvoir les regarder ultérieurement?',
             'type'=>'A',
-            'choix'=>json_encode(['Oui','Non'])   
+            'choix'=>json_encode(['Oui','Non']),
+            'sondage_id'=>1   
         ]);
         DB::table('questions')->insert([
             'corps'=>'Aimeriez vous jouer à des jeux exclusifs sur votre Bigscreen?',
             'type'=>'A',
-            'choix'=>json_encode(['Oui','Non'])  
+            'choix'=>json_encode(['Oui','Non']),
+            'sondage_id'=>1  
         ]);
         DB::table('questions')->insert([
             'corps'=>'Quelle nouvelle fonctionnalité de vos rêve devrait exister sur Bigscreen?',
             'type'=>'B',
-            'choix'=>''   
+            'choix'=>'',
+            'sondage_id'=>1   
         ]);
-      }
-}
-
     }
 }
+
